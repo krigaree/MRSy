5f51a22f3f1410c290674929315869e1c90bb645
chmjelek <44984456+chmjelek@users.noreply.github.com>
2019-01-15 23:50:15 +0100

    Add files via upload

--- Lab3/lab_3_2.m
+++ Lab3/lab_3_2.m
@@ -1,14 +1,14 @@
 clear, clc;
-#dane wejœciowe
+%dane wejœciowe
 a=0;
 b=1;
-n=input('Podaj liczbe wezlow: ');
+n=1000;
 h = (b-a)/(n-1);
 Ua = 0;
 Ub = 1;
 f = @(x) 12*x;
 g = @(x) 2*x.^3-x;
-#obliczenia
+%obliczenia
 v1 = -2*diag(eye(n));
 v2 = diag(eye(n-1));
 A = diag(v1) + diag(v2,1) + diag(v2,-1);
@@ -20,8 +20,8 @@ F(1) = F(1) - Ua;
 F(n) = F(n) - Ub;
 U = linsolve(A,F');
 U = [Ua U' Ub];
-#wykres
+%wykres
 plot(x3, g(x3), x2, g(x2), 'ro')
 legend('Metoda Analityczna','Metoda Numeryczna',0);
-#error
+%error
 E = max(abs(g(x2) - U));
